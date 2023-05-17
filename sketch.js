242cdf85187c398ea1e45f36b45591d83b3fc19f
cnrad <conradwcrawford@gmail.com>
2023-05-17 13:22:38 -0400

    something idk

--- sketch.js
+++ sketch.js
@@ -1,27 +1,35 @@
+let points = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
+  noiseDetail(1);
+
+  let space = 20;
+
+  for(let x = 0; x < windowWidth; x += space) {
+    for(let y = 0; y < windowHeight; y += space) {
+      let p = createVector(x, y);
+      points.push(p);
+    }
+  }
 }
 
 function draw() {
+  translate(-windowWidth / 2, -windowHeight / 2);
   background(0);
-  noFill();
+  strokeWeight(2);
 
-  rotateX(60);
+  for(let i = 0; i < points.length; i++) {
+    let n = noise(points[i].x, points[i].y);
+    let nx = cos(frameCount * n);
+    let ny = sin(frameCount * n);
 
-  for (let i = 0; i < 20; i++) {
-    stroke(255);
+    let r = map(sin(n * frameCount / 2), -1, 1, 0, 255);
+    let g = map(cos(frameCount), -1, 1, 0, 255);
+    let b = map(cos(frameCount ** n), -1, 1, 0, 255);
+    stroke(r, g, b, 100);
 
-    beginShape();
-    for (let j = 0; j < 360; j++) {
-      let rad = i * 10;
-
-      let x = rad * cos(j);
-      let y = rad * sin(j);
-      let z = sin(frameCount * 2 + i * 10) * 50;
-
-      vertex(x, y, z);
-    }
-    endShape(CLOSE);
+    line(points[i].x, points[i].y, map(nx, -1, 1, -20, 20) + points[i].x, map(ny + cos(frameCount), -1, 1, -20, 20) + points[i].y);
   }
 }
