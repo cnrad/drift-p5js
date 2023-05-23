b32abc073445feb7be61e3ef2c732e8ea9589d26
cnrad <conradwcrawford@gmail.com>
2023-05-23 00:26:54 -0400

    solid foundation built, implement ts later

--- sketch.js
+++ sketch.js
@@ -1,35 +1,68 @@
-let points = [];
+var inc = 0.05;
+var incStart = 0.025;
+var magInc = 0.01;
+var start = 0;
+var scl = 25;
+var cols, rows;
+var zoff = 0;
+var magOff = 0;
+var magnitude = 8;
+var slowRate = 5;
+var maxWidth = 15;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  angleMode(DEGREES);
-  noiseDetail(1);
-
-  let space = 20;
-
-  for(let x = 0; x < windowWidth; x += space) {
-    for(let y = 0; y < windowHeight; y += space) {
-      let p = createVector(x, y);
-      points.push(p);
-    }
-  }
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(2);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  background(0);
 }
 
 function draw() {
-  translate(-windowWidth / 2, -windowHeight / 2);
   background(0);
-  strokeWeight(2);
+  
+  var yoff = 0;
+  for (let y = 0; y < rows; y++) {
+    let xoff = start;
+    for (let x = 0; x < cols; x++) {
 
-  for(let i = 0; i < points.length; i++) {
-    let n = noise(points[i].x, points[i].y);
-    let nx = cos(frameCount * n);
-    let ny = sin(frameCount * n);
+      let r = map(noise(xoff, yoff, zoff), 0, 1, 0, 255);
+      let g = map(noise(xoff + 100, yoff + 100, zoff), 0, 1, 0, 255);
+      let b = map(noise(xoff + 200, yoff + 200, zoff), 0, 1, 0, 255);
 
-    let r = map(sin(n * frameCount / 2), -1, 1, 0, 255);
-    let g = map(cos(frameCount), -1, 1, 0, 255);
-    let b = map(cos(frameCount ** n), -1, 1, 0, 255);
-    stroke(r, g, b, 100);
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle);
+      let m = map(noise(xoff, yoff, magOff), 0, 1, magnitude * -1, magnitude);
+      
+      // push() and pop() for individualized transforms
+      push();
+      blendMode(ADD);
+      stroke(r, g, b);
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      let endpoint = abs(m) * scl;
 
-    line(points[i].x, points[i].y, map(nx, -1, 1, -20, 20) + points[i].x, map(ny + cos(frameCount), -1, 1, -20, 20) + points[i].y);
+      strokeWeight(map(abs(m), 0, magnitude, 1, maxWidth));
+
+      // Gradient lines, for opacity
+      let gradient = drawingContext.createLinearGradient(0, 0, endpoint, 0);
+      gradient.addColorStop(0, color(r, g, b, 0));
+      gradient.addColorStop(1, color(r, g, b, map(abs(m), 0, 1, 0, 200)));
+      drawingContext.strokeStyle = gradient;
+
+      line(0, 0, endpoint, 0);
+      // Add the little point at the end of a line, if the magnitude is lower
+      stroke(r, g, b, map(abs(m), 0, magnitude / 2, 75, 0));
+      point(endpoint, 0);
+      
+      pop();
+
+      xoff += inc;
+    }
+    yoff += inc;
   }
+
+  magOff += magInc / slowRate;
+  zoff += incStart / slowRate;
+  
 }
